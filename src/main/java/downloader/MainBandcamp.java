334bd9b1469487996bb6f1935b5e00ce6e97f0a2
Amir Aboueldahab <amir.aboueldahab@faktorzehn.de>
2020-05-07 23:01:35 +0200

    Maven Assembly Plugin

--- src/main/java/downloader/MainBandcamp.java
+++ src/main/java/downloader/MainBandcamp.java
@@ -6,9 +6,9 @@ import java.net.URL;
 public class MainBandcamp {
 
 	public static void main(String[] args) throws IOException {
+		String bandcampUrl = args[0];
 		String saveFileDirectory = System.getProperty("user.home") + "/Downloads/Privat/Musik";
-		
-		URL url = new URL("https://tops.bandcamp.com/album/i-feel-alive");
+		URL url = new URL(bandcampUrl);
 		DownloadFiles.runDownload(url, saveFileDirectory);
 	}
 
