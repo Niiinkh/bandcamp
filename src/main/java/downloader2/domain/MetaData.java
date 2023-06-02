e38f3c1972ee627173a2096d3d002d6b4b2c4369
Amir Aboueldahab <amir.aboueldahab@faktorzehn.de>
2023-06-02 10:28:12 +0200

    MetaData als Domain Object erzeugt

---
+++ src/main/java/downloader2/domain/MetaData.java
@@ -0,0 +1,52 @@
+package downloader2.domain;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.time.LocalDate;
+
+public class MetaData {
+
+    private final Track track;
+    private final Album album;
+
+    public MetaData(Track track, Album album) {
+        this.track = track;
+        this.album = album;
+    }
+
+    public String trackNumber() {
+        return String.valueOf(track.trackNumber());
+    }
+
+    public String title() {
+        return track.title();
+    }
+
+    public String artist() {
+        if (StringUtils.isNotEmpty(track.artist()))
+            return track.artist();
+        if (StringUtils.isNotEmpty(album.getArtist()))
+            return album.getArtist();
+        return "unknown artist";
+    }
+
+    public String album() {
+        return album.getTitle();
+    }
+
+    public String year() {
+        LocalDate releaseDate = album.getReleaseDate();
+        return releaseDate == null ? null : String.valueOf(releaseDate.getYear());
+    }
+
+    @Override
+    public String toString() {
+        return "MetaData{" +
+                "trackNumber=" + trackNumber() +
+                ", title=" + title() +
+                ", artist=" + artist() +
+                ", album=" + album() +
+                ", year=" + year() +
+                '}';
+    }
+}
---
+++ src/test/java/downloader2/domain/MetaDataTest.java
@@ -0,0 +1,82 @@
+package downloader2.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MetaDataTest {
+
+    private Album album;
+
+    @BeforeEach
+    void setup() {
+        album = new Album();
+    }
+
+    @Test
+    void trackNumber() {
+        Track track = new Track(5, "Penny Lane", "Beatles, the", "");
+        MetaData metaData = new MetaData(track, album);
+        assertThat(metaData.trackNumber()).isEqualTo("5");
+    }
+
+    @Test
+    void title() {
+        Track track = new Track(5, "Penny Lane", "Beatles, the", "");
+        MetaData metaData = new MetaData(track, album);
+        assertThat(metaData.title()).isEqualTo("Penny Lane");
+    }
+
+    @Test
+    void artistFromTrackIfPresent() {
+        Track track = new Track(5, "Penny Lane", "Beatles, the", "");
+        album.setArtist("the Beatles");
+        MetaData metaData = new MetaData(track, album);
+        assertThat(metaData.artist()).isEqualTo("Beatles, the");
+    }
+
+    @Test
+    void artistFromAlbumIfTrackArtistIsNull() {
+        Track track = new Track(5, "Penny Lane", null, "");
+        album.setArtist("the Beatles");
+        MetaData metaData = new MetaData(track, album);
+        assertThat(metaData.artist()).isEqualTo("the Beatles");
+    }
+
+    @Test
+    void artistFromAlbumIfTrackArtistIsEmpty() {
+        Track track = new Track(5, "Penny Lane", "", "");
+        album.setArtist("the Beatles");
+        MetaData metaData = new MetaData(track, album);
+        assertThat(metaData.artist()).isEqualTo("the Beatles");
+    }
+
+    @Test
+    void artistUnknown() {
+        Track track = new Track(5, "Penny Lane", "", "");
+        album.setArtist("");
+        MetaData metaData = new MetaData(track, album);
+        assertThat(metaData.artist()).isEqualTo("unknown artist");
+    }
+
+    @Test
+    void album() {
+        Track track = new Track(5, "Penny Lane", "Beatles, the", "");
+        album.setTitle("Magical Mystery Tour");
+        MetaData metaData = new MetaData(track, album);
+        assertThat(metaData.album()).isEqualTo("Magical Mystery Tour");
+    }
+
+
+    @Test
+    void year() {
+        Track track = new Track(5, "Penny Lane", "Beatles, the", "");
+        album.setReleaseDate(LocalDate.of(1967, 12, 8));
+        MetaData metaData = new MetaData(track, album);
+        assertThat(metaData.year()).isEqualTo("1967");
+    }
+
+}
