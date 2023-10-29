5f679d5b6fdd18bdc49e37c8cf75bffb6dafcd2e
Amir Aboueldahab <amir.aboueldahab@faktorzehn.de>
2023-10-29 10:23:08 +0100

    Updated dependencies to most current version

--- src/main/java/downloader/BandcampApp.java
+++ src/main/java/downloader/BandcampApp.java
@@ -15,20 +15,7 @@ import static downloader.adapter.out.downloader.FileSystemUtil.homeDirectory;
 public class BandcampApp {
 
     public static void main(String[] args) throws IOException {
-        /*
-        https://jackterriclothfoundation.bandcamp.com/releases;
-        https://theduesseldorfduesterboys.bandcamp.com/album/duo-duo
-        https://bigthief.bandcamp.com/album/dragon-new-warm-mountain-i-believe-in-you
-        https://spanishlovesongs.bandcamp.com/album/schmaltz
-        https://trikont.bandcamp.com/album/malva-das-grell-in-meinem-kopf
-        https://theduesseldorfduesterboys.bandcamp.com/album/im-winter-ep
-        https://theduesseldorfduesterboys.bandcamp.com/album/nenn-mich-musik
-        https://internationalmusicband.bandcamp.com/album/ententraum
-        https://internationalmusicband.bandcamp.com/album/mein-schweiss
-        https://trikont.bandcamp.com/album/philip-bradatsch
-        */
-
-        String url = "https://theduesseldorfduesterboys.bandcamp.com/album/duo-duo";
+        String url = "https://adriannelenker.bandcamp.com/album/hours-were-the-birds";
 
         InformationFetcher albumInfo = new BandcampInformationFetcher(new JsoupWrapper());
         Album album = albumInfo.fetch(url);
